eec5af6d1652498b890d412c40bd99944c1d4431
farid <dekfarid2015@gmail.com>
2025-04-28 16:04:15 +0700

    done AHHHHHHH

--- Pertemuan04/Praktikum/config/cors.go
+++ Pertemuan04/Praktikum/config/cors.go
@@ -3,6 +3,8 @@ package config
 var allowedOrigins = []string{
 	"http://localhost:3000",
 	"https://example.com",
+	"http://localhost:5174/",
+	"http://localhost:5173/",
 }
 
 func GetAllowedOrigins() []string {
